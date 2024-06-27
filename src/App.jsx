1b9768ed1a4f478d4f1ba83c7101db010e5051de
Subhi C <99975184+Subhi-c@users.noreply.github.com>
2024-06-27 19:50:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Maincontent from "./pages/Maincontent.jsx"
+function App() {
+
+  return (
+    <>
+      <Maincontent />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Maincontent.jsx
@@ -0,0 +1,9 @@
+import Joke from "../components/Joke.jsx"
+function Maincontent(){
+    return <> 
+        <div>
+            <Joke />
+        </div>
+    </>
+}
+export default Maincontent;
