f36590584869372e6c3cfe5b48affc223f2ced3d
Subhi-c <csubhiksha23@gmail.com>
2024-07-03 07:23:41 +0530

    Bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-import Maincontent from "./pages/Maincontent.jsx"
+import "./App.css";
+import firebase from "./firebase.js";
+import Maincontent from "./pages/Maincontent.jsx";
 function App() {
-
   return (
     <>
       <Maincontent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import axios from "axios";
-import { useState,useEffect } from "react";
-function Joke(){
-    const [joke, setjoke] = useState([]);
-    const [type, settype] = useState("");
-    const [category,setcategory]= useState("");
-    const typeArr = ["single", "twopart"];
-    const categoryArr = ["Programming", "Misc" ,"Spooky","Pun"];
-    useEffect(() => {
-        const url = `https://v2.jokeapi.dev/joke/Programming?type=single`;
-          axios
-            .get(url)
-            .then(data => setjoke(data.data))
-            .catch(error => console.log(error));
-      }, []); 
-      useEffect(() => {
-        if (category && type) {
-          const url = `https://v2.jokeapi.dev/joke/${category}?type=${type}`;
-          axios
-            .get(url)
-            .then(data => setjoke(data.data))
-            .catch(error => console.log(error));
-        }
-      }, [category, type]);
-    
-      function handleJoke() {
-        let i = Math.floor(Math.random() * categoryArr.length);
-        let j = Math.floor(Math.random() * typeArr.length);
-        setcategory(categoryArr[i]);
-        settype(typeArr[j]);
-      }  
-    return <> 
-        <div className="joke">
+import { useState, useEffect } from "react";
+function Joke() {
+  const [joke, setjoke] = useState([]);
+  const [type, settype] = useState("");
+  const [category, setcategory] = useState("");
+  const typeArr = ["single", "twopart"];
+  const categoryArr = ["Programming", "Misc", "Spooky", "Pun"];
+  useEffect(() => {
+    handleJoke();
+  }, []);
+  useEffect(() => {
+    if (category && type) {
+      const url = `https://v2.jokeapi.dev/joke/${category}?type=${type}`;
+      axios
+        .get(url)
+        .then((data) => setjoke(data.data))
+        .catch((error) => console.log(error));
+    }
+  }, [category, type]);
+
+  function handleJoke() {
+    let i = Math.floor(Math.random() * categoryArr.length);
+    let j = Math.floor(Math.random() * typeArr.length);
+    setcategory(categoryArr[i]);
+    settype(typeArr[j]);
+  }
+  return (
+    <>
+      <div className="joke">
+        <div>
+          {type === "single" ? (
+            <p>{joke.joke}</p>
+          ) : (
             <div>
-            {type === "single" ? (
-                <p>{joke.joke}</p>
-                ) : (
-                <div>
-                    <p>{joke.setup}</p>
-                    <p>{joke.delivery}</p>
-                </div>)}
+              <p>{joke.setup}</p>
+              <p>{joke.delivery}</p>
             </div>
-            <button onClick={handleJoke}>Next Joke</button>
+          )}
         </div>
+        <button onClick={handleJoke}>Next Joke</button>
+      </div>
     </>
+  );
 }
 export default Joke;
