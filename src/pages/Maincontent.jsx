e99974ea488159c7477791136b70a3179faa4ae9
Subhi C <99975184+Subhi-c@users.noreply.github.com>
2024-07-10 12:59:18 +0530

    Bug fixes

--- src/pages/Maincontent.jsx
+++ src/pages/Maincontent.jsx
@@ -3,6 +3,7 @@ function Maincontent(){
     return <> 
         <div>
             <Joke />
+           <p className="blk">Developed by <a href="https://github.com/Subhi-c">Subhi.C</a></p>
         </div>
     </>
 }
